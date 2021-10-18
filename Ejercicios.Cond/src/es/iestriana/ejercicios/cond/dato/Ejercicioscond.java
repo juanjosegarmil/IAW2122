39f004104782eeec0481118c53a9e020c69d775d
juanjosegarmil <a_juagarmil@iestriana.com>
2021-10-18 13:58:52 +0200

    qwd

--- Ejercicios.Cond/src/es/iestriana/ejercicios/cond/dato/Ejercicioscond.java
+++ Ejercicios.Cond/src/es/iestriana/ejercicios/cond/dato/Ejercicioscond.java
@@ -75,6 +75,32 @@ public class Ejercicioscond {
 		} else {
 			System.out.println("Error número fuera de rango (0-10)");
 		}
+		switch (num1) {
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+			System.out.println("insuficiente");
+			break;
+		case 5:
+			System.out.println("suficiente");
+			break;
+		case 6:
+			System.out.println("bien");
+			break;
+		case 7:
+		case 8:
+			System.out.println("notable");
+			break;
+		case 9:
+		case 10:
+			System.out.println("sobresaliente");
+			break;
+		default:
+			System.out.println("Nota incorrecta");
+			break;
+		}
 		sc.close();
 	}
 		
